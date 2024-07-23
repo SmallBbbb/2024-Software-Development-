a0406e582a99e2dcd7f91b737414531bc6d3c8c9
engulf408 <3370996020@qq.com>
2024-07-23 20:44:37 +0800

    Delete src/main/webapp directory

--- src/main/webapp/user_html/user_js/4_pet_task_submit.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('myDate').addEventListener('input', function() {
-    var inputDate = this.value;
-    //   var formattedDate = inputDate.split('-').reverse().join('-');
-    // document.getElementById('formattedDate').innerText = formattedDate;
-    var formattedDate = inputDate.split('-').join('-');
-    document.getElementById('formattedDate').value = formattedDate;
-});
